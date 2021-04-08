6e3d827907bdc0b46f82a3c24852c59455f2056e
Annastasya Putri Ivory <64578529+annastasyaivory@users.noreply.github.com>
2021-04-08 20:11:07 +0700

    memperbaiki tampilan halaman detail

--- lib/pages/movie_detail.dart
+++ lib/pages/movie_detail.dart
@@ -20,20 +20,46 @@ class MovieDetail extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text(movie.title),
+          backgroundColor: Colors.black,
         ),
         body: SingleChildScrollView(
             child: Center(
-                child: Column(
-          children: [
-            Container(
-                padding: EdgeInsets.all(16),
-                height: height / 1.5,
-                child: Image.network(path)),
-            Container(
-              child: Text(movie.overview),
-              padding: EdgeInsets.only(left: 16, right: 16),
-            )
-          ],
+                child: Container(
+          decoration: buildBoxDecoration(),
+          margin: EdgeInsets.all(15),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(16),
+                  height: height / 1.5,
+                  child: Image.network(path)),
+              Container(
+                child: Text(
+                  movie.overview,
+                  style: TextStyle(color: Colors.white),
+                ),
+                padding: EdgeInsets.only(left: 16, right: 16),
+              )
+            ],
+          ),
         ))));
+  }
+
+  BoxDecoration buildBoxDecoration() {
+    return BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          //jumlah stop berbanding lurus dengan jumlah warna
+          stops: [0.3, 0.6, 0.9],
+          colors: [
+            Color.fromRGBO(0, 0, 0, 1), //red-green-blue-o
+            Color.fromRGBO(105, 105, 105, 1),
+            Color.fromRGBO(128, 128, 128, 1),
+          ],
+        ),
+        boxShadow: []);
   }
 }
