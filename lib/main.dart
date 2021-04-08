600b1ec38536315767d68778906a13f2ec8d17b6
Annastasya Putri Ivory <64578529+annastasyaivory@users.noreply.github.com>
2021-04-08 15:07:15 +0700

    tambah pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_katalogfilm/pages/movie_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MovieList();
   }
 }
