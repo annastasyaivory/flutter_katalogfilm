9b85733aafd2e24d53a9b0d5843683f121144dcd
Annastasya Putri Ivory <64578529+annastasyaivory@users.noreply.github.com>
2021-04-08 16:55:44 +0700

    membuat halaman list populer movie

--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -7,7 +7,7 @@ class HttpService {
   final String apiKey = '19585cba4491f17348bf5b890678c44f';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
 
-  Future<String> getPopularMovies() async {
+  Future<List> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
